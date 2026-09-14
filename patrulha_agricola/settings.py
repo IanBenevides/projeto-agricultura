94af570eeaa45b29ef309b1f8a43fc04d6cf9891
Ian Benevides <ian.benevides.abreu@gmail.com>
2026-09-14 13:16:42 -0300

    feat: add core project settings, views, URLs, and base templates

--- patrulha_agricola/settings.py
+++ patrulha_agricola/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-(jrp$gqko=1+#k#hcnubl#r^vt_+m49w!+av$_*q5nsldy-2ao
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.60']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
